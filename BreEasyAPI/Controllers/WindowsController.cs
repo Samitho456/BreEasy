355cba27c8cecf0c7200bef3a0d272beab3340e8
AgentBlank66 <watchandlearn626@gmail.com>
2025-11-26 11:11:34 +0100

    Windows-Conrtoller methodes

--- BreEasyAPI/Controllers/WindowsController.cs
+++ BreEasyAPI/Controllers/WindowsController.cs
@@ -18,8 +18,6 @@ namespace BreEasyAPI.Controllers
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<IActionResult> Get() // 1. Change return type to async Task<IActionResult>
         {
-            Console.WriteLine("test");
-
             try
             {
                 // 2. Add 'await' here to extract the data from the Task
@@ -35,7 +33,7 @@ namespace BreEasyAPI.Controllers
         }
 
         [HttpPost]
-        public IActionResult Post(Window window)
+        public IActionResult Post([FromBody] Window window)
         {
             try
             {
@@ -46,7 +44,62 @@ namespace BreEasyAPI.Controllers
             {
                 return NotFound();
             }
-            
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            try
+            {
+                Window window = await _repo.GetById(id);
+                return Ok(window);
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPost("location/{id}")]
+        public async Task<IActionResult> GetByLocation(int id)
+        {
+            try
+            {
+                Window window = await _repo.GetByLocation(id);
+                return Ok(window);
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                Window removedWindow = await _repo.Remove(id);
+                return Ok(removedWindow);
+            }
+            catch
+            {
+                return NotFound();
+            }
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] Window window)
+        {
+            try
+            {
+                Window updatedWindow = await _repo.Update(id, window);
+                return Ok(updatedWindow);
+            }
+            catch
+            {
+                return NotFound();
+            }
         }
     }
 }
