ee059186be3fb72bbe636031ac37d005dbb6bab4
AgentBlank66 <watchandlearn626@gmail.com>
2025-11-27 09:14:07 +0100

    Creation of unitTest and methods in unitTest

---
+++ BreEasyTests/WindowRepoTests.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BreEasy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreEasy.Tests
+{
+    [TestClass()]
+    public class WindowRepoTests
+    {
+        [TestMethod()]
+        public void WindowRepoTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetByIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetByLocationTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void RemoveTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
