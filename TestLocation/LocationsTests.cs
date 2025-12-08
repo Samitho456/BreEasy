e691851f6148b9b684197ffb7e01e1eb44755dc8
RasmusRaavig <rasmusraavig@gmail.com>
2025-12-08 12:06:51 +0100

    Test of Location class

---
+++ TestLocation/LocationsTests.cs
@@ -0,0 +1,32 @@
+﻿using BreEasy;
+using BreEasy.EFDbContext;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestLocation
+{
+    [TestClass]
+    public class LocaionsTests
+    {
+        [TestMethod]
+        public void TestLocationName()
+        {
+            var location = new Location();
+            location.LocationName = "TestLocation";
+            Assert.AreEqual("TestLocation", location.LocationName);
+            Assert.AreNotEqual("WrongLocation", location.LocationName);
+            Assert.IsNotNull(location.LocationName);
+            Assert.ThrowsException<ArgumentNullException>(() => location.LocationName = null);
+        }
+
+        [TestMethod]
+        public void TestHumidity()
+        {
+            var location = new Location();
+            location.Humidity = 55.5;
+            Assert.AreEqual(55.5, location.Humidity);
+            Assert.AreNotEqual(60.0, location.Humidity);
+            Assert.IsTrue(location.Humidity >= 0 && location.Humidity <= 100);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => location.Humidity = -10);
+        }
+    }
+}
