03ad2bc1651d0196b32d7cd7173e84586a428751
OKronborg00 <oliverkronborg00@gmail.com>
2025-12-11 14:08:37 +0100

    Add TimeToClose property and Weather class
    - Introduced `TimeToClose` property in `Window.cs` to track the time until the window closes, with updates to the constructor for initialization.
    - Added a new `Weather` class in `Weather.cs` with properties for `Id`, `TemperatureCelsius`, `Humidity`, `RainAmountMM`, and `TimestampUtc`, including constructors for property initialization.

---
+++ BreEasy/Weather.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BreEasy
+{
+    public class Weather
+    {
+        public int Id { get; set; }
+        public double TemperatureCelsius { get; set; }
+        public double Humidity { get; set; }
+
+        // Tilføjet for at matche ekstern API-logik
+        public double RainAmountMM { get; set; }
+
+        public DateTime TimestampUtc { get; set; } // Retningsmæssigt mere korrekt navn
+
+        public Weather(int id, double temperatureCelsius, double humidity, double rainAmount, DateTime timestamp)
+        {
+            Id = id;
+            TemperatureCelsius = temperatureCelsius;
+            Humidity = humidity;
+            RainAmountMM = rainAmount;
+            TimestampUtc = timestamp;
+        }
+
+        public Weather() { }
+    }
+}
--- BreEasy/Window.cs
+++ BreEasy/Window.cs
@@ -35,14 +35,17 @@
         // Boolean indicating if the window is currently open or closed
         public bool IsOpen { get; set; }
 
+        public int TimeToClose { get; set; }
+
         // Constructor
-        public Window(int id, string windowName, int locationId , DateTime timeLastOpened, bool isOpen)
+        public Window(int id, string windowName, int locationId , DateTime timeLastOpened, bool isOpen, int timeToClose)
         {
             Id = id;
             WindowName = windowName;
             LocationId = locationId;
             TimeLastOpened = timeLastOpened;
             IsOpen = isOpen;
+            TimeToClose = timeToClose;
         }
 
         // Default constructor
